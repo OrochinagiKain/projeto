9066f65b1b48c8296f634991471278f63de49684
Levy Oliveira <levyoliveira0@gmail.com>
2020-11-11 16:27:53 -0400

    reversing

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json());
 
 const templatePath =
-  "https://firebasestorage.googleapis.com/v0/b/projeto-b599b.appspot.com/o/template.html?alt=media&token=e61dcf66-11c4-4443-91a6-1d3a6216cdd4";
+  "https://firebasestorage.googleapis.com/v0/b/projeto-b599b.appspot.com/o/template.html?alt=media&token=538fdad3-7f02-4ec4-a7cc-24c21a16d74e";
 
 app.post("/certificado", async (req, res) => {
   const certificadoText = await fetch(templatePath).then((res) => res.text());
